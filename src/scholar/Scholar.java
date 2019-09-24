b2791ad2d58984cef824f36f69f2d919690c6ca4
unknown <PC@ASUS-X505B-W10N.ASUS-X505B-W10N>
2019-09-24 12:46:09 -0500

    Jose Torrenegra

--- src/scholar/Scholar.java
+++ src/scholar/Scholar.java
@@ -5,31 +5,33 @@ import java.util.Scanner;
 public class Scholar {
 
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado();
-        empleado1.nombre = "Jose ";
-        empleado1.apellido = "Torrenegra";
-        empleado1.fecha_nacimiento = " 2002 ";
-        empleado1.CalcEdad();
-        System.out.println("el empleado se llama " + empleado1.nombre + empleado1.apellido);
-        System.out.println("la fecha de nacimiento es " + empleado1.fecha_nacimiento);
-        System.out.println("la edad es" + empleado1.CalcEdad () );
+        Ventana formulario=new Ventana();
+        formulario.setVisible(true);
+        //Empleado empleado1 = new Empleado();
+       //empleado1.nombre = "Jose ";
+        //empleado1.apellido = "Torrenegra";
+        //empleado1.fecha_nacimiento = " 2002 ";
+        //empleado1.CalcEdad();
+        //System.out.println("el empleado se llama " + empleado1.nombre + empleado1.apellido);
+        //System.out.println("la fecha de nacimiento es " + empleado1.fecha_nacimiento);
+        //System.out.println("la edad es" + empleado1.CalcEdad () );
         
-        Scanner lectura=new Scanner (System.in);
-        int edad, año_nacimiento, año_actual;
-        System.out.println("2019");
-        año_nacimiento=lectura.nextInt();
-        System.out.println("2002");
-        año_actual=lectura.nextInt();
-        edad=año_actual-año_nacimiento;
-        System.out.println(" "  );
+        //Scanner lectura=new Scanner (System.in);
+        //int edad, año_nacimiento, año_actual;
+        //System.out.println("2019");
+        //año_nacimiento=lectura.nextInt();
+        //System.out.println("2002");
+        //año_actual=lectura.nextInt();
+        //edad=año_actual-año_nacimiento;
+        //System.out.println(" "  );
         
 
-        Estudiante estudiante1 = new Estudiante();
-        estudiante1.nombre = " jose ";
-        estudiante1.apellido = " torrenegra ";
-        estudiante1.genero = " macho ";
-        System.out.println("el Estudiante se llama " + estudiante1.nombre + estudiante1.apellido);
-        System.out.println(" el Estudiante es " + estudiante1.genero);
+        //Estudiante estudiante1 = new Estudiante();
+        //estudiante1.nombre = " jose ";
+        //estudiante1.apellido = " torrenegra ";
+        //estudiante1.genero = " macho ";
+        //System.out.println("el Estudiante se llama " + estudiante1.nombre + estudiante1.apellido);
+        //System.out.println(" el Estudiante es " + estudiante1.genero);
         //System.out.println("el Estudiante se llama " + estudiante1.nombre + estudiante1.apellido); 
     }              //System.out.println(" el Estudiante es " + estudiante1.genero)
 
--- src/scholar/Ventana.java
+++ src/scholar/Ventana.java
@@ -1,31 +1,45 @@
 
 package scholar;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
     
    public class Ventana extends JFrame{
+    private Object getContentPane;
        
       
        
        public Ventana(){
        
-           JFrame f = new JFrame("hello work");
-           this.setSize(400,300);
-           this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           this.setVisible(true);
+           JFrame f = new JFrame("---Hola Alien Del Area 51 que se Escapo---");
+           f.setSize(400,300);
+           f.setLocationRelativeTo(null);
+           f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           f.setVisible(true);
            Container cp = getContentPane();
+           GridLayout gl = new GridLayout(4,3);
+           gl.setHgap(5); gl.setVgap(5);
            cp.setLayout(new FlowLayout());
            Jlabel etiqueta = new Jlabel("Nombre: ");
-           
+           JTexField texto = new JTexField(20);
+           JButton boton = new JButton("Saludar");
+           cp.add( etiqueta );
+           cp.add( texto );
+           cp.add(boton);
    
        
        }
 
+
+    }
+
    
    
 
-   }
+   
